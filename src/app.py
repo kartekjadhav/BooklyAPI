abdf8c026f4d0ad25e49a28f331659a2d5970cb6
SID <kartekjadhav2810@gmail.com>
2026-04-25 12:06:04 +0530

    Moved error handlers in error.errors.py

--- src/app.py
+++ src/app.py
@@ -5,19 +5,7 @@ from src.routes import book_router, auth_router, review_router
 from contextlib import asynccontextmanager
 from src.db.db import init_db
 from src.redis.redis import token_blocklist
-from src.errors.errors import (
-    BookAlreadyExists, 
-    BookNotFound, 
-    UserAlreadyExists, 
-    UserNotFound, 
-    InsufficientPrivileges, 
-    InvalidCredentials, 
-    InvalidOrExpiredToken, 
-    InvalidOrRevokedToken,
-    ReviewNotFound,
-    create_exception_handler
-)
-
+from src.errors.errors import register_all_errors
 
 REDIS_CONNECTION_RETRIES = 6
 
@@ -46,113 +34,8 @@ async def life_span(app:FastAPI):
     print("Server is stopping 🔴")
 
 app = FastAPI(lifespan=life_span, title="Bookly", description="A REST API for book review web service", version="v1")
-
-app.add_exception_handler(
-    InvalidCredentials,
-    create_exception_handler(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail={
-            "message": "Invalid email or password",
-            "error_code": "invalid_credentials"
-        }
-    )
-)
-
-app.add_exception_handler(
-    BookAlreadyExists,
-    create_exception_handler(
-        status_code=status.HTTP_409_CONFLICT,
-        detail={
-            "message": "Book already exists",
-            "error_code": "book_already_exists"
-        }
-    )
-)
-
-app.add_exception_handler(
-    BookNotFound,
-    create_exception_handler(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail={
-            "message": "Book not found",
-            "error_code": "book_not_found"
-        }
-    )
-)
-
-app.add_exception_handler(
-    UserAlreadyExists,
-    create_exception_handler(
-        status_code=status.HTTP_409_CONFLICT,
-        detail={
-            "message": "User with this email already exists",
-            "error_code": "user_already_exists"
-        }
-    )
-)
-
-app.add_exception_handler(
-    UserNotFound,
-    create_exception_handler(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail={
-            "message": "User not found",
-            "error_code": "user_not_found"
-        }
-    )
-)
-
-app.add_exception_handler(
-    InsufficientPrivileges,
-    create_exception_handler(
-        status_code=status.HTTP_403_FORBIDDEN,
-        detail={
-            "message": "Insufficient Privileges",
-            "error_code": "insufficient_privileges"
-        }
-    )
-)
-
-app.add_exception_handler(
-    InvalidOrExpiredToken,
-    create_exception_handler(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail={
-            "message": "Invalid or expired token",
-            "error_code": "invalid_token"
-        }
-    )
-)
-
-app.add_exception_handler(
-    InvalidOrRevokedToken,
-    create_exception_handler(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail={
-            "message": "Invalid or revoked token",
-            "error_code": "invalid_or_revoked_token"
-        }
-    )
-)
-
-app.add_exception_handler(
-    ReviewNotFound,
-    create_exception_handler(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail={
-            "message": "Review not found",
-            "error_code": "review_not_found"
-        }
-    )
-)
-
-@app.exception_handler(500)
-async def internal_server_error(request: Request, exc):
-    return JSONResponse(
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        content={"message": f"Oops! Something went wrong.", "error_code": "internal_server_error"}
-    )   
-
+   
+register_all_errors(app)
 
 # User auth Router
 app.include_router(auth_router, prefix="/api/v1/auth", tags=["auth"])
--- src/errors/errors.py
+++ src/errors/errors.py
@@ -1,3 +1,4 @@
+from fastapi import FastAPI, status
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
 from typing import Callable, Any
@@ -50,3 +51,110 @@ def create_exception_handler(status_code: int, detail: Any) -> Callable[[Request
         )
     
     return exception_handler
+
+def register_all_errors(app: FastAPI):
+    app.add_exception_handler(
+    InvalidCredentials,
+    create_exception_handler(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail={
+            "message": "Invalid email or password",
+            "error_code": "invalid_credentials"
+        }
+    )
+)
+
+    app.add_exception_handler(
+        BookAlreadyExists,
+        create_exception_handler(
+            status_code=status.HTTP_409_CONFLICT,
+            detail={
+                "message": "Book already exists",
+                "error_code": "book_already_exists"
+            }
+        )
+    )
+
+    app.add_exception_handler(
+        BookNotFound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={
+                "message": "Book not found",
+                "error_code": "book_not_found"
+            }
+        )
+    )
+
+    app.add_exception_handler(
+        UserAlreadyExists,
+        create_exception_handler(
+            status_code=status.HTTP_409_CONFLICT,
+            detail={
+                "message": "User with this email already exists",
+                "error_code": "user_already_exists"
+            }
+        )
+    )
+
+    app.add_exception_handler(
+        UserNotFound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={
+                "message": "User not found",
+                "error_code": "user_not_found"
+            }
+        )
+    )
+
+    app.add_exception_handler(
+        InsufficientPrivileges,
+        create_exception_handler(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail={
+                "message": "Insufficient Privileges",
+                "error_code": "insufficient_privileges"
+            }
+        )
+    )
+
+    app.add_exception_handler(
+        InvalidOrExpiredToken,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={
+                "message": "Invalid or expired token",
+                "error_code": "invalid_token"
+            }
+        )
+    )
+
+    app.add_exception_handler(
+        InvalidOrRevokedToken,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={
+                "message": "Invalid or revoked token",
+                "error_code": "invalid_or_revoked_token"
+            }
+        )
+    )
+
+    app.add_exception_handler(
+        ReviewNotFound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={
+                "message": "Review not found",
+                "error_code": "review_not_found"
+            }
+        )
+    )
+
+    @app.exception_handler(500)
+    async def internal_server_error(request: Request, exc):
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"message": f"Oops! Something went wrong.", "error_code": "internal_server_error"}
+        )
